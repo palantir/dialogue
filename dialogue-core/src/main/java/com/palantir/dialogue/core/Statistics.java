4bdc20775d058a047d57c479a46243521a2a1369
Dan Fox <dfox@palantir.com>
2020-02-11 20:32:53 +0000

    Statistics sink interface

---
+++ dialogue-core/src/main/java/com/palantir/dialogue/core/Statistics.java
@@ -0,0 +1,44 @@
+/*
+ * (c) Copyright 2020 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.dialogue.core;
+
+import com.palantir.dialogue.Endpoint;
+import com.palantir.dialogue.Request;
+import com.palantir.dialogue.Response;
+import org.immutables.value.Value;
+
+interface Statistics {
+
+    /** Returns a statisticId, to allow us to record info at the beginning and end of a request. */
+    long recordStart(Upstream upstream, Endpoint endpoint, Request request, Object from);
+
+    void recordSuccess(long statisticId, Response request);
+
+    void recordFailure(long statisticId, Throwable throwable);
+
+    // TODO(dfox): allow recording more detailed statistics about the body upload  / download time?
+
+    /**
+     * Represents a connection to some upstream server. Should be OK to have multiple upstreams pointing
+     * to one host if they have different cipher suites / proxy info / timeouts / protocol specs (h1/h2).
+     */
+    @Value.Immutable
+    interface Upstream {
+        @Value.Parameter
+        String uri();
+    }
+}
