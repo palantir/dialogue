ec2bebfda9a9884bfa84b5027c8029d31cdb4f29
Dan Fox <dfox@palantir.com>
2020-02-19 02:47:59 +0000

    simulate concurrencylimiters+blacklist+round_robin

--- simulation/src/test/java/com/palantir/dialogue/core/Strategy.java
+++ simulation/src/test/java/com/palantir/dialogue/core/Strategy.java
@@ -23,11 +23,12 @@ import com.palantir.dialogue.Channel;
 import com.palantir.dialogue.Endpoint;
 import com.palantir.dialogue.Request;
 import com.palantir.dialogue.Response;
-import com.palantir.tritium.metrics.registry.DefaultTaggedMetricRegistry;
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.function.BiFunction;
+import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
@@ -35,9 +36,10 @@ import org.slf4j.LoggerFactory;
 
 @SuppressWarnings("ImmutableEnumChecker")
 public enum Strategy {
-    CONCURRENCY_LIMITER(Strategy::concurrencyLimiter),
-    PIN_UNTIL_ERROR(Strategy::pinUntilError),
-    ROUND_ROBIN(Strategy::roundRobin);
+    CONCURRENCY_LIMITER_ROUND_ROBIN(Strategy::concurrencyLimiter),
+    CONCURRENCY_LIMITER_BLACKLIST_ROUND_ROBIN(Strategy::concurrencyLimiterBlacklistRoundRobin),
+    CONCURRENCY_LIMITER_PIN_UNTIL_ERROR(Strategy::pinUntilError),
+    UNLIMITED_ROUND_ROBIN(Strategy::roundRobin);
 
     private static final Logger log = LoggerFactory.getLogger(Strategy.class);
     private final BiFunction<Simulation, Supplier<List<SimulationServer>>, Channel> getChannel;
@@ -52,29 +54,33 @@ public enum Strategy {
 
     private static Channel concurrencyLimiter(Simulation sim, Supplier<List<SimulationServer>> channelSupplier) {
         return RefreshingChannelFactory.RefreshingChannel.create(channelSupplier, channels -> {
-            List<LimitedChannel> limitedChannels1 = channels.stream()
-                    .map(c1 -> new ConcurrencyLimitedChannel(
-                            c1, () -> ConcurrencyLimitedChannel.createLimiter(sim.clock())))
+            List<LimitedChannel> limitedChannels =
+                    channels.stream().map(concurrencyLimiter(sim)).collect(Collectors.toList());
+            LimitedChannel limited1 = new RoundRobinChannel(limitedChannels);
+            return queuedChannelAndRetrying(sim, limited1);
+        });
+    }
+
+    private static Channel concurrencyLimiterBlacklistRoundRobin(
+            Simulation sim, Supplier<List<SimulationServer>> channelSupplier) {
+        return RefreshingChannelFactory.RefreshingChannel.create(channelSupplier, channels -> {
+            List<LimitedChannel> limitedChannels = channels.stream()
+                    .map(concurrencyLimiter(sim))
+                    .map(c -> new BlacklistingChannel(c, Duration.ofSeconds(1)))
                     .collect(Collectors.toList());
-            LimitedChannel limited1 = new RoundRobinChannel(limitedChannels1);
-            limited1 = instrumentClient(limited1, sim.taggedMetrics()); // just for debugging
-            Channel channel = new QueuedChannel(limited1, DispatcherMetrics.of(sim.taggedMetrics()));
-            return new RetryingChannel(channel, 4 /* ClientConfigurations.DEFAULT_MAX_NUM_RETRIES */);
+            LimitedChannel limited1 = new RoundRobinChannel(limitedChannels);
+            return queuedChannelAndRetrying(sim, limited1);
         });
     }
 
     private static Channel pinUntilError(Simulation sim, Supplier<List<SimulationServer>> channelSupplier) {
         Random psuedoRandom = new Random(3218974678L);
         return RefreshingChannelFactory.RefreshingChannel.create(channelSupplier, channels -> {
-            List<LimitedChannel> limitedChannels = channels.stream()
-                    .map(c1 -> new ConcurrencyLimitedChannel(
-                            c1, () -> ConcurrencyLimitedChannel.createLimiter(sim.clock())))
-                    .collect(Collectors.toList());
+            List<LimitedChannel> limitedChannels =
+                    channels.stream().map(concurrencyLimiter(sim)).collect(Collectors.toList());
             LimitedChannel limited = new PinUntilErrorChannel(
                     new PinUntilErrorChannel.ReshufflingNodeList(limitedChannels, psuedoRandom, sim.clock()));
-            limited = instrumentClient(limited, sim.taggedMetrics()); // just for debugging
-            Channel channel = new QueuedChannel(limited, DispatcherMetrics.of(new DefaultTaggedMetricRegistry()));
-            return new RetryingChannel(channel, 4);
+            return queuedChannelAndRetrying(sim, limited);
         });
     }
 
@@ -83,11 +89,19 @@ public enum Strategy {
             List<LimitedChannel> limitedChannels =
                     channels.stream().map(Strategy::noOpLimitedChannel).collect(Collectors.toList());
             LimitedChannel limited = new RoundRobinChannel(limitedChannels);
-            limited =
-                    instrumentClient(limited, sim.taggedMetrics()); // will always be zero due to the noOpLimitedChannel
-            Channel channel = new QueuedChannel(limited, DispatcherMetrics.of(sim.taggedMetrics()));
-            return new RetryingChannel(channel, 4 /* ClientConfigurations.DEFAULT_MAX_NUM_RETRIES */);
+            return queuedChannelAndRetrying(sim, limited);
         });
+    }
+
+    private static Function<Channel, LimitedChannel> concurrencyLimiter(Simulation sim) {
+        return channel ->
+                new ConcurrencyLimitedChannel(channel, () -> ConcurrencyLimitedChannel.createLimiter(sim.clock()));
+    }
+
+    private static Channel queuedChannelAndRetrying(Simulation sim, LimitedChannel limited) {
+        LimitedChannel limited1 = instrumentClient(limited, sim.taggedMetrics());
+        Channel channel = new QueuedChannel(limited1, DispatcherMetrics.of(sim.taggedMetrics()));
+        return new RetryingChannel(channel, 4 /* ClientConfigurations.DEFAULT_MAX_NUM_RETRIES */);
     }
 
     private static LimitedChannel instrumentClient(LimitedChannel delegate, TaggedMetrics metrics) {
