bb9a0f11a4d6387722cf3a04513e83e14b982d0e
Dan Fox <dfox@palantir.com>
2020-02-19 03:56:36 +0000

    Javadoc & multimap

--- simulation/src/main/java/com/palantir/dialogue/core/PreferLowestUtilization.java
+++ simulation/src/main/java/com/palantir/dialogue/core/PreferLowestUtilization.java
@@ -19,6 +19,8 @@ package com.palantir.dialogue.core;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ListMultimap;
+import com.google.common.collect.MultimapBuilder;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.palantir.dialogue.Endpoint;
@@ -28,22 +30,31 @@ import com.palantir.logsafe.Preconditions;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
-import java.util.TreeMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * The idea here is to keep track of how many requests are currently being served by each server, and when we're asked
+ * to execute a request, just pick the one with the fewest in flight.
+ *
+ * This is intended to be a strict improvement over the {@link RoundRobinChannel}, which can leave fast servers
+ * underutilized, as it sends the same number to both a slow and fast node.
+ *
+ * I know this implementation is expensive to calculate - just intending to evaluate the usefulness of the
+ * heuristic before any kind of optimizing e.g. power of two choices (P2C) to avoid a full sort.
+ */
 final class PreferLowestUtilization implements LimitedChannel {
     private static final Logger log = LoggerFactory.getLogger(PreferLowestUtilization.class);
 
-    // integers are indexes into the 'channels' list
-    private final LoadingCache<LimitedChannel, AtomicInteger> active =
-            Caffeine.newBuilder().maximumSize(1000).build(upstream -> new AtomicInteger());
-    private final ImmutableList<LimitedChannel> channels;
     private final Random random;
+    private final ImmutableList<LimitedChannel> channels;
+
+    /** Each AtomicInteger stores the number of active requests that a channel is currently serving. */
+    private final LoadingCache<LimitedChannel, AtomicInteger> activeRequestsPerChannel =
+            Caffeine.newBuilder().maximumSize(1000).build(upstream -> new AtomicInteger());
 
     PreferLowestUtilization(ImmutableList<LimitedChannel> channels, Random random) {
         Preconditions.checkState(channels.size() >= 2, "At least two channels required");
@@ -53,60 +64,52 @@ final class PreferLowestUtilization implements LimitedChannel {
 
     @Override
     public Optional<ListenableFuture<Response>> maybeExecute(Endpoint endpoint, Request request) {
-        if (channels.isEmpty()) {
-            return Optional.empty();
-        }
 
-        // we accumulate everything right now (which is probably quite expensive), but it allows us to move on to the
+        // we accumulate everything right now (which is quite expensive), but it allows us to move on to the
         // next-best channel if our preferred one refuses
-        Map<Integer, List<LimitedChannel>> channelsByActive = new TreeMap<>();
-        for (LimitedChannel channel : shuffleImmutableList(channels, random)) {
-            int activeRequests = active.get(channel).get();
-            channelsByActive.compute(activeRequests, (key, existing) -> {
-                if (existing == null) {
-                    List<LimitedChannel> list = new ArrayList<>();
-                    list.add(channel);
-                    return list;
-                } else {
-                    existing.add(channel);
-                    return existing;
-                }
-            });
-        }
+        ListMultimap<Integer, LimitedChannel> channelsByUtilization = sortChannelsByLowestUtilization();
 
-        log.debug(
-                "traceid={} channelsByActive={}",
-                request.headerParams().get("X-B3-TraceId"),
-                channelsByActive.keySet());
+        for (Integer activeCount : channelsByUtilization.keySet()) {
+            // multiple channels might have the same number of inflight requests
+            for (LimitedChannel channel : channelsByUtilization.get(activeCount)) {
 
-        for (Integer activeCount : channelsByActive.keySet()) {
-            List<LimitedChannel> candidates = channelsByActive.get(activeCount);
-            for (LimitedChannel channel : candidates) {
-                // log.info("time={} best={} active={}", Duration.ofNanos(clock.read()), channel, channelsByActive);
-
-                AtomicInteger atomicInteger = active.get(channel);
+                AtomicInteger atomicInteger = activeRequestsPerChannel.get(channel);
                 atomicInteger.incrementAndGet();
 
                 Optional<ListenableFuture<Response>> maybeResponse = channel.maybeExecute(endpoint, request);
+
                 if (maybeResponse.isPresent()) {
                     ListenableFuture<Response> response = maybeResponse.get();
                     response.addListener(atomicInteger::decrementAndGet, MoreExecutors.directExecutor());
                     return Optional.of(response);
                 } else {
-                    // we have to undo the atomicInteger thing we eagerly incremented.
+                    // we quickly undo the atomicInteger thing we optimistically incremented earlier
                     atomicInteger.decrementAndGet();
                 }
             }
         }
 
-        log.debug("Every channel refused {}", channelsByActive);
+        log.debug("Every channel refused {}", channelsByUtilization);
         return Optional.empty();
     }
 
+    private ListMultimap<Integer, LimitedChannel> sortChannelsByLowestUtilization() {
+        ListMultimap<Integer, LimitedChannel> channelsByUtilization =
+                MultimapBuilder.treeKeys().arrayListValues().build();
+
+        // the shuffle ensures that when nodes have the same utilization, we won't always pick the same one
+        for (LimitedChannel channel : shuffleImmutableList(channels)) {
+            int activeRequests = activeRequestsPerChannel.get(channel).get();
+            channelsByUtilization.put(activeRequests, channel);
+        }
+
+        return channelsByUtilization;
+    }
+
     /** Returns a new shuffled list, without mutating the input list (which may be immutable). */
-    private static <T> ImmutableList<T> shuffleImmutableList(List<T> sourceList, Random random) {
+    private <T> List<T> shuffleImmutableList(ImmutableList<T> sourceList) {
         List<T> mutableList = new ArrayList<>(sourceList);
         Collections.shuffle(mutableList, random);
-        return ImmutableList.copyOf(mutableList);
+        return mutableList;
     }
 }
