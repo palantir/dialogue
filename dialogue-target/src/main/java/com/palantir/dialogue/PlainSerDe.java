61c3010761586cadea17395e5dad764ec015a137
Carter Kozak <ckozak@ckozak.net>
2020-03-04 09:50:14 -0500

    Remove unused methods from PlainSerDe
    List and Set serializers:
    Generated code uses loops in order to handle nonstandard types,
    aliases and external type imports. A subset of cases could be
    updated to use these specific methods, but it would increase
    complexity of the generator, and require more specific test coverage.
    The loop-based approach has the fringe benefit of avoiding
    intermediate collection allocation, relying on the builders internal
    state instead.
    
    Optional serializers:
    The request object does not support optional values, so writes
    to the request builder always involve checking optional.isPresent
    before calling standard serializers on the result of optional.get.
    The optional serializer methods aren't currently used, nor would
    they reduce the complexity of the generated code if they were.

--- dialogue-target/src/main/java/com/palantir/dialogue/PlainSerDe.java
+++ dialogue-target/src/main/java/com/palantir/dialogue/PlainSerDe.java
@@ -20,11 +20,6 @@ import com.palantir.conjure.java.lib.SafeLong;
 import com.palantir.ri.ResourceIdentifier;
 import com.palantir.tokens.auth.BearerToken;
 import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.OptionalDouble;
-import java.util.OptionalInt;
-import java.util.Set;
 import java.util.UUID;
 
 /**
@@ -37,73 +32,19 @@ public interface PlainSerDe {
 
     String serializeBearerToken(BearerToken in);
 
-    Optional<String> serializeOptionalBearerToken(Optional<BearerToken> in);
-
-    List<String> serializeBearerTokenList(List<BearerToken> in);
-
-    List<String> serializeBearerTokenSet(Set<BearerToken> in);
-
     String serializeBoolean(boolean in);
-
-    Optional<String> serializeOptionalBoolean(Optional<Boolean> in);
-
-    List<String> serializeBooleanList(List<Boolean> in);
-
-    List<String> serializeBooleanSet(Set<Boolean> in);
 
     String serializeDateTime(OffsetDateTime in);
 
-    Optional<String> serializeOptionalDateTime(Optional<OffsetDateTime> in);
-
-    List<String> serializeDateTimeList(List<OffsetDateTime> in);
-
-    List<String> serializeDateTimeSet(Set<OffsetDateTime> in);
-
     String serializeDouble(double in);
-
-    Optional<String> serializeOptionalDouble(OptionalDouble in);
-
-    List<String> serializeDoubleList(List<Double> in);
-
-    List<String> serializeDoubleSet(Set<Double> in);
 
     String serializeInteger(int in);
 
-    Optional<String> serializeOptionalInteger(OptionalInt in);
-
-    List<String> serializeIntegerList(List<Integer> in);
-
-    List<String> serializeIntegerSet(Set<Integer> in);
-
     String serializeRid(ResourceIdentifier in);
-
-    Optional<String> serializeOptionalRid(Optional<ResourceIdentifier> in);
-
-    List<String> serializeRidList(List<ResourceIdentifier> in);
-
-    List<String> serializeRidSet(Set<ResourceIdentifier> in);
 
     String serializeSafeLong(SafeLong in);
 
-    Optional<String> serializeOptionalSafeLong(Optional<SafeLong> in);
-
-    List<String> serializeSafeLongList(List<SafeLong> in);
-
-    List<String> serializeSafeLongSet(Set<SafeLong> in);
-
     String serializeString(String in);
 
-    Optional<String> serializeOptionalString(Optional<String> in);
-
-    List<String> serializeStringList(List<String> in);
-
-    List<String> serializeStringSet(Set<String> in);
-
     String serializeUuid(UUID in);
-
-    Optional<String> serializeOptionalUuid(Optional<UUID> in);
-
-    List<String> serializeUuidList(List<UUID> in);
-
-    List<String> serializeUuidSet(Set<UUID> in);
 }
