e6341d027eac1daae815733230a0dfcb0b884957
Carter Kozak <ckozak@ckozak.net>
2020-03-24 11:32:36 -0400

    Response leak detection samples 1% of requests (down from 100%) (#554)
    Response leak detection samples 1% of requests (down from 100%)

--- dialogue-core/src/main/java/com/palantir/dialogue/core/LeakDetectingChannel.java
+++ dialogue-core/src/main/java/com/palantir/dialogue/core/LeakDetectingChannel.java
@@ -30,6 +30,7 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
+import java.util.Random;
 import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -41,19 +42,43 @@ final class LeakDetectingChannel implements Channel {
     private final Channel delegate;
     private final String channelName;
     private final DialogueClientMetrics metrics;
+    private final Random random;
+    private final float leakDetectionProbability;
 
-    LeakDetectingChannel(Channel delegate, String channelName, DialogueClientMetrics metrics) {
+    LeakDetectingChannel(
+            Channel delegate,
+            String channelName,
+            DialogueClientMetrics metrics,
+            Random random,
+            float leakDetectionProbability) {
         this.delegate = delegate;
         this.channelName = channelName;
         this.metrics = metrics;
+        this.random = random;
+        this.leakDetectionProbability = leakDetectionProbability;
     }
 
     @Override
     public ListenableFuture<Response> execute(Endpoint endpoint, Request request) {
+        ListenableFuture<Response> result = delegate.execute(endpoint, request);
+        return shouldApplyLeakDetection() ? wrap(result, endpoint) : result;
+    }
+
+    private ListenableFuture<Response> wrap(ListenableFuture<Response> result, Endpoint endpoint) {
         return Futures.transform(
-                delegate.execute(endpoint, request),
+                result,
                 response -> new LeakDetectingResponse(response, new LeakDetector(response, endpoint)),
                 MoreExecutors.directExecutor());
+    }
+
+    private boolean shouldApplyLeakDetection() {
+        if (leakDetectionProbability >= 1) {
+            return true;
+        }
+        if (leakDetectionProbability <= 0) {
+            return false;
+        }
+        return random.nextFloat() <= leakDetectionProbability;
     }
 
     /**
