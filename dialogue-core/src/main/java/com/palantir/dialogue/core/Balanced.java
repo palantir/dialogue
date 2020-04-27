ff4386db12aa6c1b18b0bcfc7c748450cc432a30
Carter Kozak <ckozak@ckozak.net>
2020-04-27 09:48:34 -0400

    30s + 10x

--- dialogue-core/src/main/java/com/palantir/dialogue/core/Balanced.java
+++ dialogue-core/src/main/java/com/palantir/dialogue/core/Balanced.java
@@ -55,7 +55,7 @@ final class Balanced implements LimitedChannel {
      * We'd like to remember failures for a long time, but this would increase CPU on a hot codepath as we compute
      * the number of failures for each channel in order to sort. See {@link ChannelWithStats#score}.
      */
-    private static final Duration FAILURE_MEMORY = Duration.ofSeconds(5);
+    private static final Duration FAILURE_MEMORY = Duration.ofSeconds(30);
 
     /**
      * This comparator is a little risky because the data can change while we're sorting. In practise the sort
@@ -130,14 +130,14 @@ final class Balanced implements LimitedChannel {
             public void onSuccess(Response result) {
                 inflight.decrementAndGet();
                 if (Responses.isQosStatus(result) || Responses.isServerError(result)) {
-                    recentFailures.update(1);
+                    recentFailures.update(10);
                 }
             }
 
             @Override
             public void onFailure(Throwable _throwable) {
                 inflight.decrementAndGet();
-                recentFailures.update(1);
+                recentFailures.update(10);
             }
         };
 
