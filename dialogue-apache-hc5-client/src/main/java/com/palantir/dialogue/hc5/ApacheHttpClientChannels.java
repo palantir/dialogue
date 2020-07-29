5ba11777e661ed6783c2ae21039f0325b6b1e6d1
Carter Kozak <ckozak@ckozak.net>
2020-07-29 13:14:33 -0400

    Dialogue explicitly sets enabled TLS protocols (#933)
    Dialogue explicitly sets enabled TLS protocols

--- dialogue-apache-hc5-client/src/main/java/com/palantir/dialogue/hc5/ApacheHttpClientChannels.java
+++ dialogue-apache-hc5-client/src/main/java/com/palantir/dialogue/hc5/ApacheHttpClientChannels.java
@@ -386,11 +386,7 @@ public final class ApacheHttpClientChannels {
             SSLSocketFactory rawSocketFactory = conf.sslSocketFactory();
             SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(
                     MetricRegistries.instrument(conf.taggedMetricRegistry(), rawSocketFactory, name),
-                    // Use 'null' to allow the supported protocols that hc5 considers secure: TLSv1.2 and (if
-                    // available) TLSv1.3.
-                    // We don't have the data to discover supported protocols directly here, which would require
-                    // either an SSLContext instance, or a Socket.
-                    null,
+                    new String[] {"TLSv1.2"},
                     supportedCipherSuites(
                             conf.enableGcmCipherSuites()
                                     ? CipherSuites.allCipherSuites()
