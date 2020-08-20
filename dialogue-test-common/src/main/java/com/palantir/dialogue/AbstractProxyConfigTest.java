7ce816fb3862f1d5a8c4fff73cc81c74b1ba03ee
Dan Fox <dfox@palantir.com>
2020-08-20 14:48:08 +0200

    InetSocketAddress.createUnresolved

--- dialogue-test-common/src/main/java/com/palantir/dialogue/AbstractProxyConfigTest.java
+++ dialogue-test-common/src/main/java/com/palantir/dialogue/AbstractProxyConfigTest.java
@@ -126,7 +126,7 @@ public abstract class AbstractProxyConfigTest {
         return new ProxySelector() {
             @Override
             public List<Proxy> select(URI _uri) {
-                InetSocketAddress addr = new InetSocketAddress(host, port);
+                InetSocketAddress addr = InetSocketAddress.createUnresolved(host, port);
                 return ImmutableList.of(new Proxy(Proxy.Type.HTTP, addr));
             }
 
