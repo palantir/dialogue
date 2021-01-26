cb3dfd655135705c004cacde3d46b2bf803035a9
jakubk <jakubk@palantir.com>
2021-01-26 13:21:31 +0000

    Cleanup

--- dialogue-target/src/main/java/com/palantir/dialogue/DefaultCallingThreadExecutor.java
+++ dialogue-target/src/main/java/com/palantir/dialogue/DefaultCallingThreadExecutor.java
@@ -55,13 +55,6 @@ final class DefaultCallingThreadExecutor implements CallingThreadExecutor {
 
     @Override
     public Future<?> submit(Runnable task) {
-        // if (Thread.currentThread().getId() == threadId) {
-        //     queue.checkNotPoisoned();
-        //     RunnableFuture<?> future = new FutureTask<>(task, null);
-        //     future.run();
-        //     return future;
-        // }
-
         return queue.submit(task);
     }
 
