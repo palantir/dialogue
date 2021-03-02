c069ae45426b3452d9c5000cc3bee114139d2e7d
Carter Kozak <ckozak@ckozak.net>
2021-03-02 09:08:44 -0500

    Annotation based solution to avoid static method string matching (#1138)
    Annotation based solution to avoid static method string matching

--- dialogue-clients/src/main/java/com/palantir/dialogue/clients/Reflection.java
+++ dialogue-clients/src/main/java/com/palantir/dialogue/clients/Reflection.java
@@ -18,6 +18,9 @@ package com.palantir.dialogue.clients;
 
 import com.palantir.dialogue.Channel;
 import com.palantir.dialogue.ConjureRuntime;
+import com.palantir.dialogue.DialogueService;
+import com.palantir.dialogue.DialogueServiceFactory;
+import com.palantir.dialogue.EndpointChannelFactory;
 import com.palantir.logsafe.Preconditions;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
@@ -37,14 +40,22 @@ final class Reflection {
         Preconditions.checkNotNull(dialogueInterface, "dialogueInterface");
         Preconditions.checkNotNull(channel, "channel");
 
+        DialogueService annotation = dialogueInterface.getAnnotation(DialogueService.class);
+        if (annotation != null) {
+            return createFromAnnotation(dialogueInterface, annotation, channel, conjureRuntime);
+        }
+
         try {
+            Optional<Method> legacyMethod = getLegacyStaticOfMethod(dialogueInterface);
+            if (legacyMethod.isPresent()) {
+                return dialogueInterface.cast(legacyMethod.get().invoke(null, channel, conjureRuntime));
+            }
             Method method = getStaticOfMethod(dialogueInterface)
                     .orElseThrow(() -> new SafeIllegalStateException(
                             "A static 'of(Channel, ConjureRuntime)' method is required",
                             SafeArg.of("dialogueInterface", dialogueInterface)));
 
-            return dialogueInterface.cast(method.invoke(null, channel, conjureRuntime));
-
+            return dialogueInterface.cast(method.invoke(null, endpointChannelFactory(channel), conjureRuntime));
         } catch (IllegalAccessException | InvocationTargetException e) {
             throw new SafeIllegalArgumentException(
                     "Failed to reflectively construct dialogue client. Please check the "
@@ -54,12 +65,63 @@ final class Reflection {
         }
     }
 
-    private static Optional<Method> getStaticOfMethod(Class<?> dialogueInterface) {
+    private static Optional<Method> getLegacyStaticOfMethod(Class<?> dialogueInterface) {
         try {
-            return Optional.ofNullable(dialogueInterface.getMethod("of", Channel.class, ConjureRuntime.class));
+            return Optional.of(dialogueInterface.getMethod("of", Channel.class, ConjureRuntime.class));
         } catch (NoSuchMethodException e) {
             log.debug("Failed to get static 'of' method", SafeArg.of("interface", dialogueInterface), e);
             return Optional.empty();
         }
+    }
+
+    private static Optional<Method> getStaticOfMethod(Class<?> dialogueInterface) {
+        try {
+            return Optional.of(dialogueInterface.getMethod("of", EndpointChannelFactory.class, ConjureRuntime.class));
+        } catch (NoSuchMethodException e) {
+            log.debug("Failed to get static 'of' method", SafeArg.of("interface", dialogueInterface), e);
+            return Optional.empty();
+        }
+    }
+
+    private static <T> T createFromAnnotation(
+            Class<T> dialogueInterface,
+            DialogueService dialogueService,
+            Channel channel,
+            ConjureRuntime conjureRuntime) {
+        Class<? extends DialogueServiceFactory<?>> serviceFactoryClass = dialogueService.value();
+        EndpointChannelFactory factory = endpointChannelFactory(channel);
+        Object client;
+        try {
+            client = serviceFactoryClass.getConstructor().newInstance().create(factory, conjureRuntime);
+        } catch (NoSuchMethodException e) {
+            throw new SafeIllegalArgumentException(
+                    "Failed to reflectively construct dialogue client. The service factory class must have a "
+                            + "public no-arg constructor.",
+                    e,
+                    SafeArg.of("dialogueInterface", dialogueInterface),
+                    SafeArg.of("serviceFactoryClass", serviceFactoryClass));
+        } catch (ReflectiveOperationException e) {
+            throw new SafeIllegalArgumentException(
+                    "Failed to reflectively construct dialogue client.",
+                    e,
+                    SafeArg.of("dialogueInterface", dialogueInterface),
+                    SafeArg.of("serviceFactoryClass", serviceFactoryClass));
+        }
+        if (dialogueInterface.isInstance(client)) {
+            return dialogueInterface.cast(client);
+        }
+        throw new SafeIllegalArgumentException(
+                "Dialogue service factory produced an incompatible service",
+                SafeArg.of("dialogueInterface", dialogueInterface),
+                SafeArg.of("serviceFactoryClass", serviceFactoryClass),
+                SafeArg.of("invalidClientType", client.getClass()),
+                SafeArg.of("invalidClient", client));
+    }
+
+    private static EndpointChannelFactory endpointChannelFactory(Channel channel) {
+        if (channel instanceof EndpointChannelFactory) {
+            return (EndpointChannelFactory) channel;
+        }
+        return endpoint -> request -> channel.execute(endpoint, request);
     }
 }
