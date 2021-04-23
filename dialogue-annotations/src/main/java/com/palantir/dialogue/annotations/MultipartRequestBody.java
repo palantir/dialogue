c15810c8b250db82a99f2ade92aa2193616ba167
jakubk <jakubk@palantir.com>
2021-04-23 12:48:24 +0100

    Start plumbing a MultipartRequestBody.

---
+++ dialogue-annotations/src/main/java/com/palantir/dialogue/annotations/MultipartRequestBody.java
@@ -0,0 +1,97 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.dialogue.annotations;
+
+import com.palantir.dialogue.RequestBody;
+import java.io.IOException;
+import java.io.OutputStream;
+import org.apache.hc.client5.http.entity.mime.AbstractContentBody;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.entity.mime.MultipartPartBuilder;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+
+/** An <a href="http://www.ietf.org/rfc/rfc2387.txt">RFC 2387</a>-compliant request body. */
+public final class MultipartRequestBody extends HttpEntityBodyRequestBodyAdapter {
+
+    private MultipartRequestBody(HttpEntity entity) {
+        super(entity);
+    }
+
+    public static final class Builder {
+
+        private final MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+        private Builder() {}
+
+        // apache has automatic detection for this, perhaps we shouldn't provide this straight away.
+        // Salt sets it to MIXED even though it only sends form values.
+        public Builder contentType(String contentType) {
+            builder.setContentType(ContentType.create(contentType));
+            return this;
+        }
+
+        public Builder addRequestBodyPart(RequestBodyPartBuilder requestBodyPartBuilder) {
+            builder.addPart(requestBodyPartBuilder.builder.build());
+            return this;
+        }
+
+        public MultipartRequestBody build() {
+            return new MultipartRequestBody(builder.build());
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static RequestBodyPartBuilder requestBodyPartBuilder(RequestBody unsafeRequestBody) {
+        return new RequestBodyPartBuilder(unsafeRequestBody);
+    }
+
+    public static final class RequestBodyPartBuilder {
+        private final MultipartPartBuilder builder;
+
+        private RequestBodyPartBuilder(RequestBody unsafeRequestBody) {
+            this.builder = MultipartPartBuilder.create(
+                    new AbstractContentBody(ContentType.parse(unsafeRequestBody.contentType())) {
+                        @Override
+                        public long getContentLength() {
+                            return -1;
+                        }
+
+                        @Override
+                        public String getFilename() {
+                            // Allowed
+                            return null;
+                        }
+
+                        @Override
+                        public void writeTo(OutputStream out) throws IOException {
+                            try (RequestBody requestBody = unsafeRequestBody) {
+                                requestBody.writeTo(out);
+                            }
+                        }
+                    });
+        }
+
+        public RequestBodyPartBuilder addHeaderValue(String key, String value) {
+            builder.addHeader(key, value);
+            return this;
+        }
+    }
+}
---
+++ dialogue-annotations/src/test/java/com/palantir/dialogue/annotations/MultipartRequestBodyTest.java
@@ -0,0 +1,143 @@
+/*
+ * (c) Copyright 2021 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.dialogue.annotations;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.net.MediaType;
+import com.palantir.dialogue.annotations.MultipartRequestBody.RequestBodyPartBuilder;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+import okhttp3.Headers;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okio.Buffer;
+import org.immutables.value.Value;
+import org.junit.jupiter.api.Test;
+
+public final class MultipartRequestBodyTest {
+
+    @Test
+    public void testCanSupportSalt() throws IOException {
+        List<SaltValue> saltValues = ImmutableList.of(
+                ImmutableSaltValue.builder()
+                        .key("key1")
+                        .bucket("bucket")
+                        .value("I am String: indeed I am")
+                        .contentType(MediaType.PLAIN_TEXT_UTF_8.toString())
+                        .build(),
+                ImmutableSaltValue.builder()
+                        .key("key2")
+                        .bucket("bucket")
+                        .value("{\"i-am-json\":\"indeed-i-am\"}")
+                        .contentType(MediaType.JSON_UTF_8.toString())
+                        .putKeyValues("If-Match", "version")
+                        .build());
+
+        MultipartBody okhttp = createOkhttpMultipartBody(saltValues);
+        Buffer buffer = new Buffer();
+        okhttp.writeTo(buffer);
+
+        MultipartRequestBody dialogue = createDialogueMultipartRequestBody(saltValues);
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        dialogue.writeTo(byteArrayOutputStream);
+
+        assertThat(byteArrayOutputStream.toString(StandardCharsets.UTF_8.name()))
+                .isEqualTo(buffer.readString(StandardCharsets.UTF_8));
+    }
+
+    private MultipartBody createOkhttpMultipartBody(List<SaltValue> saltValues) {
+        MultipartBody.Builder multipartBodyBuilder = new MultipartBody.Builder();
+        multipartBodyBuilder.setType(MultipartBody.MIXED);
+
+        for (SaltValue entry : saltValues) {
+            final String bucket = entry.bucket();
+            final String key = entry.key();
+            final String value = entry.value();
+
+            Headers headers = Headers.of(ImmutableMap.<String, String>builder()
+                    .put("bucket", bucket)
+                    .put("key", key)
+                    .putAll(entry.keyValues())
+                    .build());
+
+            RequestBody body = RequestBody.create(okhttp3.MediaType.parse(entry.contentType()), value);
+
+            multipartBodyBuilder.addPart(MultipartBody.Part.create(headers, body));
+        }
+
+        return multipartBodyBuilder.build();
+    }
+
+    private MultipartRequestBody createDialogueMultipartRequestBody(List<SaltValue> saltValues) {
+        MultipartRequestBody.Builder builder = MultipartRequestBody.builder();
+
+        for (SaltValue entry : saltValues) {
+            final String bucket = entry.bucket();
+            final String key = entry.key();
+            final String value = entry.value();
+
+            RequestBodyPartBuilder requestBodyPartBuilder =
+                    MultipartRequestBody.requestBodyPartBuilder(new com.palantir.dialogue.RequestBody() {
+                        @Override
+                        public void writeTo(OutputStream output) throws IOException {
+                            output.write(value.getBytes(StandardCharsets.UTF_8));
+                        }
+
+                        @Override
+                        public String contentType() {
+                            return entry.contentType();
+                        }
+
+                        @Override
+                        public boolean repeatable() {
+                            return false;
+                        }
+
+                        @Override
+                        public void close() {}
+                    });
+            requestBodyPartBuilder.addHeaderValue("bucket", bucket);
+            requestBodyPartBuilder.addHeaderValue("key", key);
+            entry.keyValues().forEach(requestBodyPartBuilder::addHeaderValue);
+            builder.addRequestBodyPart(requestBodyPartBuilder);
+        }
+
+        return builder.build();
+    }
+
+    @Value.Immutable
+    @Value.Style(stagedBuilder = true)
+    interface SaltValue {
+        String key();
+
+        String bucket();
+
+        String value();
+
+        Map<String, String> keyValues();
+
+        String contentType();
+    }
+}
