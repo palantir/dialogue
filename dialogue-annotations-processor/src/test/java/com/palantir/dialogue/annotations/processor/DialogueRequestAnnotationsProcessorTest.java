63ae35a7d636d85292f9e79b9da4350d39ff4b2d
Carter Kozak <ckozak@ckozak.net>
2021-05-26 11:35:44 -0400

    Remove unnecessary sourceCompatibility overrides

--- dialogue-annotations-processor/src/test/java/com/palantir/dialogue/annotations/processor/DialogueRequestAnnotationsProcessorTest.java
+++ dialogue-annotations-processor/src/test/java/com/palantir/dialogue/annotations/processor/DialogueRequestAnnotationsProcessorTest.java
@@ -97,7 +97,6 @@ public final class DialogueRequestAnnotationsProcessorTest {
                 clazz.getSimpleName() + ".java"));
         try {
             return Compiler.javac()
-                    .withOptions("-source", "11")
                     .withProcessors(new DialogueRequestAnnotationsProcessor())
                     .compile(JavaFileObjects.forResource(clazzPath.toUri().toURL()));
         } catch (MalformedURLException e) {
