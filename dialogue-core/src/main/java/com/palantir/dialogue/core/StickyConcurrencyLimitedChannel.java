05eb1a2b82e7be8b4a6a0faaebd57a52da35990a
jakubk <jakubk@palantir.com>
2021-07-20 20:50:09 +0100

    Spotless

--- dialogue-core/src/main/java/com/palantir/dialogue/core/StickyConcurrencyLimitedChannel.java
+++ dialogue-core/src/main/java/com/palantir/dialogue/core/StickyConcurrencyLimitedChannel.java
@@ -63,9 +63,7 @@ final class StickyConcurrencyLimitedChannel implements LimitedChannel {
             Optional<ListenableFuture<Response>> result = delegate.maybeExecute(
                     endpoint,
                     request,
-                    permit.isOnlyInFlight()
-                            ? LimitEnforcement.DANGEROUS_BYPASS_LIMITS
-                            : limitEnforcement);
+                    permit.isOnlyInFlight() ? LimitEnforcement.DANGEROUS_BYPASS_LIMITS : limitEnforcement);
             if (result.isPresent()) {
                 DialogueFutures.addDirectCallback(result.get(), permit);
                 return result;
