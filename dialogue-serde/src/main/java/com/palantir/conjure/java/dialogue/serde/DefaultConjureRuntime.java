e76f29c3621dcc5f5bdcb088b07f22dc54a84c2d
David Schlosnagle <davids@palantir.com>
2022-02-14 09:25:48 -0500

    weakValues

--- dialogue-serde/src/main/java/com/palantir/conjure/java/dialogue/serde/DefaultConjureRuntime.java
+++ dialogue-serde/src/main/java/com/palantir/conjure/java/dialogue/serde/DefaultConjureRuntime.java
@@ -33,7 +33,7 @@ import java.util.List;
 public final class DefaultConjureRuntime implements ConjureRuntime {
     @VisibleForTesting
     static final CaffeineSpec DEFAULT_SERDE_CACHE_SPEC =
-            CaffeineSpec.parse("maximumSize=1000,expireAfterAccess=1m,softValues");
+            CaffeineSpec.parse("maximumSize=1000,expireAfterAccess=1m,weakValues");
 
     static final ImmutableList<WeightedEncoding> DEFAULT_ENCODINGS = ImmutableList.of(
             WeightedEncoding.of(Encodings.json(), .9),
