2dc700d7230aadfb455f307a1b1d01b0fe63cd6c
Brian Laub <blaub@palantir.com>
2024-02-21 16:33:18 -0500

    fix tests

--- dialogue-clients/src/test/java/com/palantir/dialogue/clients/ProtocolVersionFilteringDialogueDnsResolverTest.java
+++ dialogue-clients/src/test/java/com/palantir/dialogue/clients/ProtocolVersionFilteringDialogueDnsResolverTest.java
@@ -17,12 +17,10 @@
 package com.palantir.dialogue.clients;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableSetMultimap;
 import com.palantir.dialogue.core.DialogueDnsResolver;
+import com.palantir.dialogue.util.MapBasedDnsResolver;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import org.junit.jupiter.api.Test;
@@ -31,48 +29,39 @@ class ProtocolVersionFilteringDialogueDnsResolverTest {
 
     @Test
     void returns_only_ipv4_when_input_contains_only_ipv4() throws UnknownHostException {
-        ImmutableSet<InetAddress> resolvedAddresses = ImmutableSet.of(
-                InetAddress.getByAddress(new byte[] {1, 1, 1, 1}), InetAddress.getByAddress(new byte[] {2, 2, 2, 2}));
+        InetAddress address1 = InetAddress.getByAddress(new byte[] {1, 1, 1, 1});
+        InetAddress address2 = InetAddress.getByAddress(new byte[] {2, 2, 2, 2});
 
-        DialogueDnsResolver delegate = mock(DialogueDnsResolver.class);
-        when(delegate.resolve(anyString())).thenReturn(resolvedAddresses);
-
+        DialogueDnsResolver delegate =
+                new MapBasedDnsResolver(ImmutableSetMultimap.of("foo.com", address1, "foo.com", address2));
         DialogueDnsResolver resolver = new ProtocolVersionFilteringDialogueDnsResolver(delegate);
 
-        assertThat(resolver.resolve("foo")).isEqualTo(resolvedAddresses);
+        assertThat(resolver.resolve("foo.com")).containsExactly(address1, address2);
     }
 
     @Test
     void returns_only_ipv6_when_input_contains_only_ipv6() throws UnknownHostException {
-        ImmutableSet<InetAddress> resolvedAddresses = ImmutableSet.of(
-                InetAddress.getByName("0fa7:be4f:a4d2:4e33:22d0:c4b6:7ddb:19f0"),
-                InetAddress.getByName("2b08:8eff:13b5:1246:5208:acb1:6b52:648a"));
+        InetAddress address1 = InetAddress.getByName("0fa7:be4f:a4d2:4e33:22d0:c4b6:7ddb:19f0");
+        InetAddress address2 = InetAddress.getByName("2b08:8eff:13b5:1246:5208:acb1:6b52:648a");
 
-        DialogueDnsResolver delegate = mock(DialogueDnsResolver.class);
-        when(delegate.resolve(anyString())).thenReturn(resolvedAddresses);
-
+        DialogueDnsResolver delegate =
+                new MapBasedDnsResolver(ImmutableSetMultimap.of("foo.com", address1, "foo.com", address2));
         DialogueDnsResolver resolver = new ProtocolVersionFilteringDialogueDnsResolver(delegate);
 
-        assertThat(resolver.resolve("foo")).isEqualTo(resolvedAddresses);
+        assertThat(resolver.resolve("foo.com")).containsExactly(address1, address2);
     }
 
     @Test
     void returns_only_ipv4_when_input_contains_mix_of_ipv4_and_ipv6() throws UnknownHostException {
-        ImmutableSet<InetAddress> resolvedIpv4Addresses = ImmutableSet.of(
-                InetAddress.getByAddress(new byte[] {1, 1, 1, 1}), InetAddress.getByAddress(new byte[] {2, 2, 2, 2}));
-        ImmutableSet<InetAddress> resolvedIpv6Addresses = ImmutableSet.of(
-                InetAddress.getByName("0fa7:be4f:a4d2:4e33:22d0:c4b6:7ddb:19f0"),
-                InetAddress.getByName("2b08:8eff:13b5:1246:5208:acb1:6b52:648a"));
-        ImmutableSet<InetAddress> resolvedAddresses = ImmutableSet.<InetAddress>builder()
-                .addAll(resolvedIpv4Addresses)
-                .addAll(resolvedIpv6Addresses)
-                .build();
+        InetAddress address1v4 = InetAddress.getByAddress(new byte[] {1, 1, 1, 1});
+        InetAddress address2v4 = InetAddress.getByAddress(new byte[] {2, 2, 2, 2});
+        InetAddress address1v6 = InetAddress.getByName("0fa7:be4f:a4d2:4e33:22d0:c4b6:7ddb:19f0");
+        InetAddress address2v6 = InetAddress.getByName("2b08:8eff:13b5:1246:5208:acb1:6b52:648a");
 
-        DialogueDnsResolver delegate = mock(DialogueDnsResolver.class);
-        when(delegate.resolve(anyString())).thenReturn(resolvedAddresses);
-
+        DialogueDnsResolver delegate = new MapBasedDnsResolver(ImmutableSetMultimap.of(
+                "foo.com", address1v4, "foo.com", address1v6, "foo.com", address2v4, "foo.com", address2v6));
         DialogueDnsResolver resolver = new ProtocolVersionFilteringDialogueDnsResolver(delegate);
 
-        assertThat(resolver.resolve("foo")).isEqualTo(resolvedIpv4Addresses);
+        assertThat(resolver.resolve("foo.com")).containsExactly(address1v4, address2v4);
     }
 }
