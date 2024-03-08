487bfe1a664ccb3a019b549cbaff14a11978fdd7
Carter Kozak <ckozak@palantir.com>
2024-03-08 09:29:19 -0500

    DefaultDialogueDnsResolver logs cached failures at debug, others at info

--- dialogue-clients/src/main/java/com/palantir/dialogue/clients/DefaultDialogueDnsResolver.java
+++ dialogue-clients/src/main/java/com/palantir/dialogue/clients/DefaultDialogueDnsResolver.java
@@ -51,12 +51,25 @@ final class DefaultDialogueDnsResolver implements DialogueDnsResolver {
             return ImmutableSet.copyOf(results);
         } catch (UnknownHostException e) {
             GaiError gaiError = extractGaiError(e, hostname);
-            log.warn(
-                    "Unknown host '{}'. {}: {}",
-                    SafeArg.of("gaiErrorType", gaiError.name()),
-                    SafeArg.of("gaiErrorMessage", gaiError.errorMessage()),
-                    UnsafeArg.of("hostname", hostname),
-                    e);
+            if (gaiError == GaiError.CACHED) {
+                // Cached results can be fairly noisy, avoid heavy logging.
+                if (log.isDebugEnabled()) {
+                    log.debug(
+                            "Unknown host '{}'. {}: {}",
+                            SafeArg.of("gaiErrorType", gaiError.name()),
+                            SafeArg.of("gaiErrorMessage", gaiError.errorMessage()),
+                            UnsafeArg.of("hostname", hostname),
+                            e);
+                }
+            } else {
+                // Sometimes UnknownHostException is reasonable/expected, so we log at info rather than warn.
+                log.info(
+                        "Unknown host '{}'. {}: {}",
+                        SafeArg.of("gaiErrorType", gaiError.name()),
+                        SafeArg.of("gaiErrorMessage", gaiError.errorMessage()),
+                        UnsafeArg.of("hostname", hostname),
+                        e);
+            }
             metrics.failure(gaiError.name()).mark();
             return ImmutableSet.of();
         }
