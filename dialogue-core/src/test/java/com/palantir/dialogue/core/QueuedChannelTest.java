1568290f30c9ddafb41ac43e59d83ef79527395d
Carter Kozak <ckozak@palantir.com>
2024-11-15 10:44:05 -0500

    complete rewrite of QueuedChannelTest

--- dialogue-core/src/test/java/com/palantir/dialogue/core/QueuedChannelTest.java
+++ dialogue-core/src/test/java/com/palantir/dialogue/core/QueuedChannelTest.java
@@ -18,73 +18,32 @@ package com.palantir.dialogue.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
-import com.palantir.dialogue.Endpoint;
 import com.palantir.dialogue.Request;
 import com.palantir.dialogue.Response;
 import com.palantir.dialogue.TestEndpoint;
 import com.palantir.dialogue.TestResponse;
-import com.palantir.dialogue.core.LimitedChannel.LimitEnforcement;
 import com.palantir.dialogue.core.QueuedChannel.QueuedChannelInstrumentation;
+import com.palantir.dialogue.futures.DialogueFutures;
 import com.palantir.tracing.TestTracing;
 import com.palantir.tritium.metrics.registry.DefaultTaggedMetricRegistry;
+import java.time.Duration;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
-import org.junit.jupiter.api.BeforeEach;
+import java.util.concurrent.atomic.AtomicReference;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
-import org.mockito.stubbing.OngoingStubbing;
 
 @ExtendWith(MockitoExtension.class)
 @SuppressWarnings("FutureReturnValueIgnored")
 public class QueuedChannelTest {
-
-    private static final LimitEnforcement DO_NOT_SKIP_LIMITS = LimitEnforcement.DEFAULT_ENABLED;
-
-    @Mock
-    private LimitedChannel delegate;
-
-    @Mock
-    private Endpoint endpoint;
-
-    @Mock
-    private Request request;
-
-    @Mock
-    private Response mockResponse;
-
-    private QueuedChannel queuedChannel;
-    private SettableFuture<Response> futureResponse;
-    private Optional<ListenableFuture<Response>> maybeResponse;
-
-    @BeforeEach
-    public void before() {
-        queuedChannel = new QueuedChannel(
-                delegate,
-                "my-channel",
-                "queue-type",
-                QueuedChannel.channelInstrumentation(
-                        DialogueClientMetrics.of(new DefaultTaggedMetricRegistry()), "my-channel"),
-                100_000);
-        futureResponse = SettableFuture.create();
-        maybeResponse = Optional.of(futureResponse);
-
-        mockHasCapacity();
-    }
 
     private static QueuedChannel createQueue(LimitedChannel delegate) {
         return createQueue(delegate, 100_000);
@@ -175,30 +134,44 @@ public class QueuedChannelTest {
     }
 
     @Test
-    public void testQueuedRequestExecutedOnNextSubmission_throws() throws ExecutionException, InterruptedException {
-        // First request is limited by the channel and queued
-        Request queuedRequest = Mockito.mock(Request.class);
-        when(delegate.maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS)).thenReturn(Optional.empty());
-        ListenableFuture<Response> queuedFuture =
-                queuedChannel.maybeExecute(endpoint, queuedRequest).get();
-        verify(delegate, times(2)).maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS);
-        assertThat(queuedFuture).isNotDone();
+    public void testQueuedRequestExecutedOnNextSubmission_throws() {
+        List<Optional<SettableFuture<Response>>> settableResponses = new CopyOnWriteArrayList<>();
+        AtomicBoolean delegateThrows = new AtomicBoolean(false);
+        LimitedChannel delegateChannel = (_endpoint, _request, limitEnforcement) -> {
+            if (delegateThrows.get()) {
+                throw new NullPointerException("expected");
+            }
+            Optional<SettableFuture<Response>> result = Optional.empty();
+            if (!limitEnforcement.enforceLimits()) {
+                result = Optional.of(SettableFuture.create());
+            }
+            settableResponses.add(result);
+            return result.map(item -> item);
+        };
+        QueuedChannel queued = createQueue(delegateChannel);
 
-        // Second request succeeds and the queued request is attempted, but throws an exception
-        futureResponse.set(mockResponse);
-        when(delegate.maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS)).thenReturn(maybeResponse);
-        when(delegate.maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS))
-                .thenThrow(new NullPointerException("expected"));
-        ListenableFuture<Response> completed =
-                queuedChannel.maybeExecute(endpoint, request).get();
-        // Both results should be completed. The thrown exception should
-        // be converted into a failed future by NeverThrowLimitedChannel
-        assertThat(completed).isDone();
-        assertThat(queuedFuture).isDone();
-        assertThat(completed.get()).isEqualTo(mockResponse);
-        assertThatThrownBy(queuedFuture::get).hasRootCauseMessage("expected");
-        verify(delegate, times(1)).maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS);
-        verify(delegate, times(3)).maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS);
+        assertThat(settableResponses).isEmpty();
+
+        // Initial request is expected to be allowed in all cases, to allow the queue to be processed
+        assertThat(queued.maybeExecute(TestEndpoint.GET, Request.builder().build()))
+                .hasValueSatisfying(item -> assertThat(item).isNotDone());
+        assertThat(settableResponses).hasSize(1);
+        assertThat(settableResponses.get(0))
+                .hasValueSatisfying(item -> assertThat(item).isNotDone());
+
+        Optional<ListenableFuture<Response>> queuedFuture =
+                queued.maybeExecute(TestEndpoint.GET, Request.builder().build());
+        assertThat(queuedFuture).isPresent();
+        assertThat(settableResponses).hasSize(3);
+
+        delegateThrows.set(true);
+        queued.schedule();
+        assertThat(queuedFuture).hasValueSatisfying(item -> assertThat(item)
+                .failsWithin(Duration.ZERO)
+                .withThrowableThat()
+                .havingRootCause()
+                .isInstanceOf(NullPointerException.class)
+                .withMessage("expected"));
     }
 
     @Test
@@ -364,27 +337,53 @@ public class QueuedChannelTest {
     }
 
     @Test
-    public void testQueuedResponseClosedOnCancel() {
-        Request queuedRequest =
-                Request.builder().pathParams(ImmutableMap.of("foo", "bar")).build();
-        when(delegate.maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS)).thenReturn(Optional.empty());
-        ListenableFuture<Response> result =
-                queuedChannel.maybeExecute(endpoint, queuedRequest).get();
-        verify(delegate, times(2)).maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS);
+    public void testQueuedResponseClosedOnCancel() throws Exception {
+        List<Optional<SettableFuture<TestResponse>>> settableResponses = new CopyOnWriteArrayList<>();
+        AtomicBoolean hasCapacity = new AtomicBoolean(false);
+        AtomicReference<ListenableFuture<?>> queuedReturnedFuture = new AtomicReference<>();
+        LimitedChannel delegateChannel = (_endpoint, req, limitEnforcement) -> {
+            Optional<SettableFuture<TestResponse>> result = Optional.empty();
+            if (hasCapacity.get() || !limitEnforcement.enforceLimits()) {
+                result = Optional.of(SettableFuture.create());
+            }
+            settableResponses.add(result);
+            ListenableFuture<?> queuedReturnedFutureValue = queuedReturnedFuture.get();
+            if (result.isPresent()
+                    && req.headerParams().containsKey("cancel-me")
+                    && queuedReturnedFutureValue != null) {
+                // When the above preconditions are met, we cancel the outer QueuedChannel future which delegates
+                // to this response in order to ensure this response is correctly closed when a cancellation race
+                // occurs.
+                queuedReturnedFutureValue.cancel(true);
+                result.get().set(new TestResponse().code(200));
+            }
+            return result.map(item -> DialogueFutures.transform(item, value -> value));
+        };
+        QueuedChannel queued = createQueue(delegateChannel);
 
-        when(delegate.maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS))
-                .thenReturn(Optional.of(Futures.immediateFuture(Mockito.mock(Response.class))));
-        when(delegate.maybeExecute(endpoint, queuedRequest, DO_NOT_SKIP_LIMITS))
-                .thenAnswer((Answer<Optional<ListenableFuture<Response>>>) _invocation -> {
-                    // cancel from this invocation to simulate the race between cancellation and execution
-                    assertThat(result.cancel(true)).isTrue();
-                    return Optional.of(Futures.immediateFuture(mockResponse));
-                });
-        // Force scheduling
-        queuedChannel.maybeExecute(endpoint, request);
-        assertThat(result).isCancelled();
-        verify(delegate, times(1)).maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS);
-        verify(mockResponse, times(1)).close();
+        assertThat(settableResponses).isEmpty();
+
+        // Initial request is expected to be allowed in all cases, to allow the queue to be processed
+        assertThat(queued.maybeExecute(TestEndpoint.GET, Request.builder().build()))
+                .hasValueSatisfying(item -> assertThat(item).isNotDone());
+        assertThat(settableResponses).hasSize(1);
+        assertThat(settableResponses.get(0))
+                .hasValueSatisfying(item -> assertThat(item).isNotDone());
+
+        // Now that we have a request in flight, we can queue one:
+        Optional<ListenableFuture<Response>> queuedResponse = queued.maybeExecute(
+                TestEndpoint.GET,
+                Request.builder().putHeaderParams("cancel-me", "true").build());
+        assertThat(queuedResponse).isPresent();
+        queuedReturnedFuture.set(queuedResponse.get());
+
+        hasCapacity.set(true);
+
+        settableResponses.get(0).get().set(new TestResponse().code(200));
+
+        assertThat(settableResponses).hasSize(4);
+        TestResponse cancelledResponse = settableResponses.get(3).get().get(0, TimeUnit.SECONDS);
+        assertThat(cancelledResponse.isClosed()).isTrue();
     }
 
     @Test
@@ -440,24 +439,47 @@ public class QueuedChannelTest {
 
     @Test
     public void testQueuedResponseAvoidsExecutingCancelled() {
-        Request queued = Request.builder().putHeaderParams("key", "val").build();
-        when(delegate.maybeExecute(endpoint, queued, DO_NOT_SKIP_LIMITS)).thenReturn(Optional.empty());
-        ListenableFuture<Response> result =
-                queuedChannel.maybeExecute(endpoint, queued).get();
-        verify(delegate, times(2)).maybeExecute(endpoint, queued, DO_NOT_SKIP_LIMITS);
+        List<Optional<SettableFuture<Response>>> settableResponses = new CopyOnWriteArrayList<>();
+        AtomicBoolean hasCapacity = new AtomicBoolean(false);
+        LimitedChannel delegateChannel = (_endpoint, req, limitEnforcement) -> {
+            Optional<SettableFuture<Response>> result = Optional.empty();
+            if (hasCapacity.get() || !limitEnforcement.enforceLimits()) {
+                result = Optional.of(SettableFuture.create());
+            }
+            if (req.headerParams().containsKey("queued")) {
+                result.ifPresent(resultFuture -> {
+                    resultFuture.setException(new AssertionError(
+                            "the queued request should be cancelled and never submitted after hasCapacity=true"));
+                });
+            }
+            settableResponses.add(result);
+            return result.map(item -> item);
+        };
+        QueuedChannel queued = createQueue(delegateChannel);
 
-        assertThat(result.cancel(true)).isTrue();
-        when(delegate.maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS))
-                .thenReturn(Optional.of(Futures.immediateFuture(Mockito.mock(Response.class))));
-        queuedChannel.maybeExecute(endpoint, request);
-        verify(delegate, times(1)).maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS);
-        // Should not have been invoked any more.
-        verify(delegate, times(2)).maybeExecute(endpoint, queued, DO_NOT_SKIP_LIMITS);
-    }
+        assertThat(settableResponses).isEmpty();
 
-    private OngoingStubbing<Optional<ListenableFuture<Response>>> mockHasCapacity() {
-        return Mockito.lenient()
-                .when(delegate.maybeExecute(endpoint, request, DO_NOT_SKIP_LIMITS))
-                .thenReturn(maybeResponse);
+        // Initial request is expected to be allowed in all cases, to allow the queue to be processed
+        assertThat(queued.maybeExecute(TestEndpoint.GET, Request.builder().build()))
+                .hasValueSatisfying(item -> assertThat(item).isNotDone());
+        assertThat(settableResponses).hasSize(1);
+        assertThat(settableResponses.get(0))
+                .hasValueSatisfying(item -> assertThat(item).isNotDone());
+
+        Optional<ListenableFuture<Response>> queuedFuture = queued.maybeExecute(
+                TestEndpoint.GET,
+                Request.builder().putHeaderParams("queued", "true").build());
+
+        assertThat(queuedFuture).isPresent();
+        assertThat(settableResponses).hasSize(3);
+
+        queuedFuture.get().cancel(true);
+        hasCapacity.set(true);
+
+        queued.schedule();
+
+        assertThat(settableResponses)
+                .as("The queued future is cancelled, and shouldn't be re-submitted")
+                .hasSize(3);
     }
 }
